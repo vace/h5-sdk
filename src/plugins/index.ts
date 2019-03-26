7432fb320d6806abde88993faf3cd5db710350df
Vace <ocdo@qq.com>
2019-03-26 16:37:16 +0800

    optimize

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,23 +1,19 @@
-import Http from '../factory/Http'
-import App from '../factory/App';
-import Res from '../factory/Res';
+import * as jssdk from './jssdk'
+import * as ui from './ui'
+import * as safety from './safety'
+import * as tool from './tool'
+import * as cloud from './cloud'
+import * as analysis from './analysis'
+import * as wechat from './wechat'
 
-import * as _jssdk from './jssdk'
-import * as _ui from './ui'
-import * as _safety from './safety'
-import * as _tool from './tool'
-import * as _cloud from './cloud'
-import * as _analysis from './analysis'
-import * as _wechat from './wechat'
+export { store } from './store'
+export {
+  jssdk,
+  ui,
+  safety,
+  tool,
+  cloud,
+  analysis,
+  wechat
+}
 
-export { store } from './store';
-export const http = Http.instance
-export const jssdk = _jssdk
-export const cloud = _cloud
-export const analysis = _analysis
-export const ui = _ui
-export const safety = _safety
-export const tool = _tool
-export const app = App.instance
-export const res = Res.instance
-export const wechat = _wechat

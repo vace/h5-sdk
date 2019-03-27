aae3da933aecdd708040cabbe691d1b54b6673fd
Vace <ocdo@qq.com>
2019-03-27 18:08:31 +0800

    change `ts` support

--- src/venders/index.ts
+++ src/venders/index.ts
@@ -3,7 +3,12 @@
 /**
  * zepto 插件，导出对象为 $,和zepto
  */
-export * from './zepto'
+import _Zepto from './zepto'
+
+//! 这里为了能更好的生成d.ts描述文件
+
+export const $: ZeptoStatic = _Zepto
+export const Zepto: ZeptoStatic = _Zepto
 
 /**
  * @namespace store

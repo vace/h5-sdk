ae58f177dc784cc234839bb9fb93c1dd524e41a9
Vace <ocdo@qq.com>
2019-05-01 16:04:39 +0800

    支持openGraph

--- src/scheduler/initialize-jssdk.ts
+++ src/scheduler/initialize-jssdk.ts
@@ -1,3 +1,13 @@
+/**
+ * 支持 解析opengraph 标签属性
+ * og:title 标题。
+ * og:type 类型。类型请使用协议官方网站上Object types定义的类型。
+ * og:image 表示页面信息的一张缩略图地址。
+ * og:url 代表页面的规范url。
+ * og:description 关于页面信息的简短描述。
+ * og:site_name 页面所在的网站名称。
+ */
+
 import { getElementAttrs } from "../utils/shared";
 import { config, share, defaultJsApiList } from "../plugins/jssdk";
 import $ from "../venders/zepto";
@@ -18,10 +28,25 @@ export default function initializeJssdk () {
     options.jsApiList = apiList.concat(defaultJsApiList)
   }
   config(options)
+
+  // 对于未设置的属性，尝试同步opengrah的设置
+  const hasOpenGraph = (opt: any, prop1: string, prop2: string) => {
+    // 已经设置则不同步opengrah
+    if (opt[prop1]) return
+    var el = document.querySelector(`meta[property="og:${prop2}"]`)
+    if (el) {
+      opt[prop1] = el.getAttribute('content')
+    }
+  }
   const $shares = $('meta[name="sdk:share"]')
   const propsShare = ['type', 'title', 'desc', 'link', 'img', 'imgUrl']
+  
   $shares.each((index, element) => {
     const options = getElementAttrs($(element), propsShare)
+    hasOpenGraph(options, 'title', 'title')
+    hasOpenGraph(options, 'desc', 'description')
+    hasOpenGraph(options, 'img', 'image')
+    hasOpenGraph(options, 'link', 'url')
     share(options)
   })
 }

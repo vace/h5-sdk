e809d1d264721872ebf60e789124ffceacfb2202
Vace <ocdo@qq.com>
2019-05-01 16:34:37 +0800

    判断是否在小程序下

--- src/utils/global.ts
+++ src/utils/global.ts
@@ -10,6 +10,11 @@ export const document = global.document
 /** @ignore Wx */
 export const getwx = () => (global as any).wx
 
+/** 是否为微信小程序内部 */
+export const isWxMini = () => {
+  return global['__wxjs_environment'] === 'miniprogram'
+}
+
 /** @ignore fetch */
 export const fetch = global.fetch
 

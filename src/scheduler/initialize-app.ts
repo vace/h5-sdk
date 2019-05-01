3310109e79450096ea8c3960889bb6ccc9120e05
Vace <ocdo@qq.com>
2019-05-01 16:05:27 +0800

    支持应用版本参数

--- src/scheduler/initialize-app.ts
+++ src/scheduler/initialize-app.ts
@@ -9,7 +9,7 @@ export default function initializeApp () {
   if (!element.length) {
     return
   }
-  const props = ['appid', 'scope', 'wxappid', 'jsappid']
+  const props = ['appid', 'scope', 'wxappid', 'jsappid', 'version']
   const options: any = getElementAttrs(element, props)
   App.getInstance().config(options).run()
 }

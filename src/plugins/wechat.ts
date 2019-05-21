9461354fb9c4f535135ae92a0ea4516c6dffffac
Vace <ocdo@qq.com>
2019-05-21 23:03:45 +0800

    移除无用代码

--- src/plugins/wechat.ts
+++ src/plugins/wechat.ts
@@ -1,7 +1,6 @@
-import { getCurrentHref } from "../utils/shared";
-import { randomstr, stringify, parse, each } from "../functions/index";
 import { location } from "../utils/global";
 import { service } from "./cloud";
+import { getAppid } from "../plugins/jssdk";
 
 /**
  * 微信能力
@@ -15,44 +14,44 @@ export function getQrcode (username: string): string {
   return `https://open.weixin.qq.com/qr/code?username=${username}`
 }
 
-type WechatSubscribeMsg = {
-  openid: string
-  template_id: string
-  action: 'confirm' | 'cancel'
-  scene: number
-  reserved: string
-}
+// type WechatSubscribeMsg = {
+//   openid: string
+//   template_id: string
+//   action: 'confirm' | 'cancel'
+//   scene: number
+//   reserved: string
+// }
 
-/** 微信一次性订阅消息 */
-export function subscribeMsg(appid: string, template_id: string, scene: number = 1): WechatSubscribeMsg | undefined {
-  if (!template_id) {
-    throw new Error('template_id cannot be empty');
-  }
-  const [host, queryStr] = getCurrentHref().split('?')
-  const _query: any = parse(queryStr)
-  const newquery = {
-    action: 'get_confirm',
-    appid,
-    scene,
-    template_id,
-    redirect_url: getCurrentHref(),
-    reserved: randomstr(16)
-  }
-  // 授权返回
-  if (_query.template_id === template_id) {
-    return _query
-  } else {
-    // 消除参数歧义
-    each(newquery, (val: any, key: string) => delete _query[key])
-    newquery.redirect_url = `${host}?${stringify(_query)}`
-    location.replace(`https://mp.weixin.qq.com/mp/subscribemsg?${stringify(newquery)}#wechat_redirect`)
-  }
-}
+/** 微信一次性订阅消息，废弃，不再由前端进行维护 */
+// export function subscribeMsg(appid: string, template_id: string, scene: number = 1): WechatSubscribeMsg | undefined {
+//   if (!template_id) {
+//     throw new Error('template_id cannot be empty');
+//   }
+//   const [host, queryStr] = getCurrentHref().split('?')
+//   const _query: any = parse(queryStr)
+//   const newquery = {
+//     action: 'get_confirm',
+//     appid,
+//     scene,
+//     template_id,
+//     redirect_url: getCurrentHref(),
+//     reserved: randomstr(16)
+//   }
+//   // 授权返回
+//   if (_query.template_id === template_id) {
+//     return _query
+//   } else {
+//     // 消除参数歧义
+//     each(newquery, (val: any, key: string) => delete _query[key])
+//     newquery.redirect_url = `${host}?${stringify(_query)}`
+//     location.replace(`https://mp.weixin.qq.com/mp/subscribemsg?${stringify(newquery)}#wechat_redirect`)
+//   }
+// }
 
-/** 获取短连接 */
-export function shorturl (appid: string, url: string): Promise<string> {
+/** 获取短连接，appid不提供则读取jsappid */
+export function shorturl (url: string, appid?: string): Promise<string> {
   return service('wechat/shorturl', {
-    wxappid: appid,
+    appid: appid || getAppid(),
     url: url || location.href
   })
 }

7035c7c4544e9be1f5534a6c45c3f75707930858
Vace <ocdo@qq.com>
2019-07-22 17:02:34 +0800

    小程序插件

---
+++ src/plugins/index.mini.ts
@@ -0,0 +1,9 @@
+import * as safety from './safety'
+export { default as store } from '../adapters/store/index'
+// @ts-ignore
+import * as ui from '../adapters/ui/ui.mini'
+
+export {
+  safety,
+  ui
+}

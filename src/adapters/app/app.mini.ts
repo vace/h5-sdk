5635a1563312b646dd5debfb97f65da0274dd033
Vace <ocdo@qq.com>
2019-07-22 16:58:22 +0800

    小程序应用适配器

---
+++ src/adapters/app/app.mini.ts
@@ -0,0 +1,53 @@
+import App from '../../factory/App'
+import Auth from '../../factory/Auth';
+import {
+  loading,
+  success,
+  error,
+  alert
+} from '../ui/ui.mini'
+import { parse, stringify } from '../../functions/qs';
+import { WxRequestCallbackResult } from '../request/request.mini';
+
+export default function createAppMini () {
+  App.showLoading = (message: any) => {
+    return loading(typeof message === 'string' ? message : '请稍后...')
+  }
+
+  App.showError = (message: any) => {
+    return error(message)
+  }
+
+  App.showSuccess = (message: any) => {
+    return success(message)
+  }
+  
+  App.transformRequest = function (this: App, option: any) {
+    const { appid, auth } = this
+    const [host, queryString = ''] = option.url.split('?')
+    const headers = <Headers>option.headers
+    // 支持 {appid}/action 的格式
+    if (host.indexOf(appid) === -1) {
+      const query = parse(queryString)
+      if (!query.appid) {
+        query.appid = appid
+        option.url = `${host}?${stringify(query)}`
+      }
+    }
+    if (auth.isAuthed && auth.isAccessTokenValid) {
+      if (!headers['Authorization']) {
+        headers['Authorization'] = <string>auth.accessToken
+      }
+    }
+    return option
+  }
+
+  App.transformResponse = (response: WxRequestCallbackResult) => {
+    const authorize = response.header['authorization'] || response.header['Authorization']
+    if (authorize) {
+      Auth.instance.saveToken(authorize)
+    }
+    return response.data
+  }
+
+}

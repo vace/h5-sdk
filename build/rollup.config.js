f95009098edbac3e6c63407258ddffb5913bd7de
Vace <ocdo@qq.com>
2019-07-22 22:22:04 +0800

    提权

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -30,53 +30,85 @@ const isWatchMode = process.env.ROLLUP_WATCH === 'true'
 
 console.log(`Make Build Plantform: ${Plantform}，Env：${BuildEnv}`)
 
-export default {
-  input: './src/web-entry.ts',
-  plugins: [
-    typescript(),
-    json(),
-    postcss({
-      plugins: [
-        autoprefixer(),
-        cssnano({ safe: true })
-      ],
-      extract: 'dist/sdk.css',
-      extensions: ['.css', '.less']
-    }),
-    resolve({ browser: true }),
-    commonjs(),
-    replace({
-      '__VERSION__': version,
-      'process.env.NODE_ENV': JSON.stringify(isDevelopment ? 'development' : 'production'),
-      // 编译平台
-      '__PLANTFORM__': Plantform
-    }),
-    isWatchMode && isDevelopment && serve({
-      port: 1235,
-      contentBase: ['dist', 'demo']
-    }),
-    isProduction && uglify({
-      output: {
-        comments: (node, { value, type }) => type == "comment2" && /h5-sdk/i.test(value)
-      }
-    })
-  ].filter(val => !!val),
-  moduleContext: {
-    'whatwg-fetch': 'window'
-  },
-  context: 'window',
-  watch: {
-    include: 'src/**',
-    exclude: 'node_modules/**'
-  },
-  context: 'window',
-  output: {
-    file: isDevelopment ? './dist/sdk.dev.js' : './dist/sdk.js',
-    format: 'umd',
-    name: 'sdk',
-    globals: {
-      'whatwg-fetch': 'fetch'
-    },
-    banner
-  }
+const configure = {}
+
+const createReplace = () => {
+  return replace({
+    '__VERSION__': version,
+    'process.env.NODE_ENV': JSON.stringify(isDevelopment ? 'development' : 'production'),
+    // 编译平台
+    '__PLANTFORM__': Plantform
+  })
 }
+
+// WEB 端
+if (Plantform === 'web') {
+  Object.assign(configure, {
+    input: './src/web-entry.ts',
+    plugins: [
+      typescript(),
+      json(),
+      postcss({
+        plugins: [
+          autoprefixer(),
+          cssnano({ safe: true })
+        ],
+        extract: 'dist/sdk.css',
+        extensions: ['.css', '.less']
+      }),
+      resolve({ browser: true }),
+      createReplace(),
+      commonjs(),
+      isWatchMode && isDevelopment && serve({
+        port: 1235,
+        contentBase: ['dist', 'demo']
+      }),
+      isProduction && uglify({
+        output: {
+          comments: (node, { value, type }) => type == "comment2" && /h5-sdk/i.test(value)
+        }
+      })
+    ].filter(val => !!val),
+    moduleContext: {
+      'whatwg-fetch': 'window'
+    },
+    context: 'window',
+    watch: {
+      include: 'src/**',
+      exclude: 'node_modules/**'
+    },
+    output: {
+      file: isDevelopment ? './dist/sdk.dev.js' : './dist/sdk.js',
+      format: 'umd',
+      name: 'sdk',
+      globals: {
+        'whatwg-fetch': 'fetch'
+      },
+      banner
+    }
+  })
+} else if (Plantform === 'mini') {
+  Object.assign(configure, {
+    input: './src/mini-entry.ts',
+    plugins: [
+      typescript(),
+      json(),
+      resolve(),
+      createReplace(),
+      commonjs()
+    ].filter(val => !!val),
+    watch: {
+      include: 'src/**',
+      exclude: 'node_modules/**'
+    },
+    context: 'window',
+    output: {
+      file: './dist/sdk.mini.js',
+      format: 'umd',
+      name: 'sdk',
+      banner
+    }
+  })
+}
+
+export default configure

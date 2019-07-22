e9ce8e590235080c5139d812ddf0a68c3a792c47
Vace <ocdo@qq.com>
2019-07-22 22:21:58 +0800

    事件改写

--- src/functions/helper.ts
+++ src/functions/helper.ts
@@ -2,15 +2,18 @@ import { document } from "../utils/global";
 
 
 /** 监听事件 */
-export function addListener(element: HTMLElement | Document, event: string, callback: EventListener): Function {
+export function addListener(element: Window | HTMLElement | Document, event: string, callback: EventListener): Function {
   element.addEventListener(event, callback, false);
   return function unbind() {
     element.removeEventListener(event, callback, false);
   };
 }
 
+// 只有在document模式下有此状态
+const isLoadComplete = !document || /comp|inter|loaded/.test(document.readyState)
+
 /** 页面是否就绪 */
-export const domready: Promise<boolean> = /comp|inter|loaded/.test(document.readyState) ? Promise.resolve(true) : new Promise((resolve) => {
+export const domready: Promise<boolean> = isLoadComplete ? Promise.resolve(true) : new Promise((resolve) => {
   const onDocumentLoaded = () => {
     unbind1()
     unbind2()

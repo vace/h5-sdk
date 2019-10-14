00b84d4c9942c71a36727aa6870eeecb07704751
Vace <ocdo@qq.com>
2019-10-14 22:46:02 +0800

    新增数组移除方法

--- src/functions/lodash.ts
+++ src/functions/lodash.ts
@@ -80,3 +80,59 @@ export function after(n: number, func: Function) {
 export function once(func: Function) {
   return before(2, func)
 }
+
+/**
+ * Removes all elements from `array` that `predicate` returns truthy for
+ * and returns an array of the removed elements. The predicate is invoked
+ * with three arguments: (value, index, array).
+ *
+ * **Note:** Unlike `filter`, this method mutates `array`. Use `pull`
+ * to pull elements from an array by value.
+ *
+ * @since 2.0.0
+ * @category Array
+ * @param {Array} array The array to modify.
+ * @param {Function} predicate The function invoked per iteration.
+ * @returns {Array} Returns the new array of removed elements.
+ * @see pull, pullAll, pullAllBy, pullAllWith, pullAt, reject, filter
+ * @example
+ *
+ * const array = [1, 2, 3, 4]
+ * const evens = remove(array, n => n % 2 == 0)
+ *
+ * console.log(array)
+ * // => [1, 3]
+ * console.log(evens)
+ * // => [2, 4]
+ */
+export function remove (array: any[], predicate: Function): any[] {
+  const result: any[] = []
+  if (!(array != null && array.length)) {
+    return result
+  }
+  let index = -1
+  const indexes: number[] = []
+  const { length } = array
+
+  while (++index < length) {
+    const value = array[index]
+    if (predicate(value, index, array)) {
+      result.push(value)
+      indexes.push(index)
+    }
+  }
+  // basePullAt(array, indexes)
+  let newLength = array ? indexes.length : 0
+  const lastIndex = length - 1
+
+  while (newLength--) {
+    let previous
+    const index = indexes[newLength]
+    if (newLength === lastIndex || index !== previous) {
+      previous = index
+      array.splice(index, 1)
+    }
+  }
+  return result
+}
+

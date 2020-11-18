7dace8a6358a1fe432285d8de0e8267cadb70f7a
Vace <ocdo@qq.com>
2020-11-18 21:21:53 +0800

    add timeout

--- src/functions/jsonp.ts
+++ src/functions/jsonp.ts
@@ -1,19 +1,23 @@
 interface IJsonpOption {
+  // 回调函数
   callback?: string
+  // 超时时间
+  timeout?: number
 }
 
 let uuid = 0
 export function jsonp (url: string, options: IJsonpOption | any = {}) {
   return new Promise((resolve, reject) => {
+    const { callback = 'callback', timeout } = options
     const root = document.head || document.body
-    let script: any = document.createElement('script')
+    const script = document.createElement('script')
     const name = '__sdkjsonp_' + uuid++
-    const callback = options.callback || 'callback'
+    const timeId = timeout ? setTimeout(() => reject(new Error('request jsonp timeout')), timeout) : null
     script.src = `${url}${/\?/.test(url) ? '&' : '?'}${callback}=${name}`
     window[name] = function (data: any) {
       root.removeChild(script)
-      script = null
       delete window[name]
+      timeId !== null && clearTimeout(timeId)
       resolve(data)
     }
     // TODO ERROR HANDLE

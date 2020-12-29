9800f48d2fe9d70efeed011780b448c5308185fe
Vace <ocdo@qq.com>
2020-12-29 22:28:47 +0800

    add `momocache`

--- src/plugins/hotcache.ts
+++ src/plugins/hotcache.ts
@@ -2,8 +2,9 @@ import store from './store'
 import { now, isDef, isArray } from '../functions/common'
 
 /**
- * 热点数据缓存
+ * 热点数据缓存，采用LRU方式，缓存固定队列数值
  */
+
 type ICacheObject = {
   key: string
   value: any
@@ -12,43 +13,63 @@ type ICacheObject = {
 
 export default function hotcache(cacheKey: string, maxLength: number = 16) {
   // 读取当前缓存项目
-  let cache: ICacheObject[]
-
-  const _lzcache: () => ICacheObject[] = () => {
-    if (!isDef(cache)) {
-      cache = store.get(cacheKey)
-      !isArray(cache) && (cache = [])
+  let hotCacheList: ICacheObject[]
+  // 当前内存缓存
+  const memocache = new Map()
+  // lzcache
+  const _getHotList: () => ICacheObject[] = () => {
+    if (!isDef(hotCacheList)) {
+      hotCacheList = store.get(cacheKey)
+      if (!isArray(hotCacheList)) {
+        hotCacheList = []
+      }
     }
-    return cache
+    return hotCacheList
   }
 
-  const _find = (key: string) => _lzcache().find(obj => obj.key === key)
-
-  const _spliced = (key) => {
+  // 查找key
+  const _find = (key: string) => _getHotList().find(obj => obj.key === key)
+  // 移除key
+  const _spliced = (key: string) => {
     const hit = _find(key)
-    return isDef(hit) && cache.splice(cache.indexOf(hit), 1)
+    return isDef(hit) && hotCacheList.splice(hotCacheList.indexOf(hit), 1)
   }
 
   // 取值
   const get = (key: string, _default?: any) => {
+    // 尝试从内存中查找
+    if (memocache.has(key)) {
+      return memocache.get(key)
+    }
     const hit = _find(key)
-    return isDef(hit) ? hit.value : _default
+    if (isDef(hit)) {
+      memocache.set(key, hit.value)
+      return hit.value
+    }
+    return _default
   }
   // 设置
   const set = (key: string, value: any) => {
     _spliced(key)
-    const cache = _lzcache()
+    const cache = _getHotList()
     cache.unshift({ key, value, hot: now() })
     // 排序后只缓存最新的值
     store.set(cacheKey, cache.slice(0, maxLength))
+    memocache.set(key, value)
     return value
   }
-
+  // 移除
   const remove = (key: string) => {
     if (_spliced(key)) {
-      store.set(cacheKey, _lzcache())
+      store.set(cacheKey, _getHotList())
+      memocache.delete(key)
     }
   }
+  // 清空
+  const clearAll = () => {
+    store.remove(cacheKey)
+    memocache.clear()
+  }
 
-  return { get, set, remove }
+  return { get, set, remove, clearAll }
 }

b6989b0f5fdc0ccedf5aa016ffcb532ada625e03
Vace <ocdo@qq.com>
2020-12-30 22:27:23 +0800

    add refresh time

--- src/factory/AuthUser.ts
+++ src/factory/AuthUser.ts
@@ -1,12 +1,15 @@
 import Auth from './Auth'
 import hotcache from '../plugins/hotcache'
-import { isObject, each } from '../functions/common'
+import { isObject, each, timestamp } from '../functions/common'
 
 const AuthSymbol = Symbol('auth')
 const UserStore = hotcache('@SdkUsers')
 
 export default class AuthUser {
-
+  /** 用户资料刷新时间 */
+  public static REFRESH_TIME = 5 * 24 * 3600
+  /** 用户登录时间 */
+  private $logintime: number = 0
   /** 用户ID */
   public id: number = 0
   /** 当前平台 */
@@ -33,39 +36,45 @@ export default class AuthUser {
   public location: string = ''
   /** 同一主体下的ID */
   public unionid: string = ''
-
+  /** 绑定的auth实例 */
   private [AuthSymbol]: Auth
-
   /** 原始数据 */
   public get data () {
     return UserStore.get(this.$key)
   }
-
+  /** 缓存key */
   get $key() {
     return this[AuthSymbol].$key
   }
+  /** 是否登录 */
   get isLogin() {
     return this.id > 0
   }
-
+  /** 需要刷新资料 */
+  get needRefreshed () {
+    return this.$logintime < timestamp() - AuthUser.REFRESH_TIME
+  }
+  /** 实例化auth用户 */
   public constructor(auth: Auth) {
     this[AuthSymbol] = auth
     const user = UserStore.get(this.$key)
     if (isObject(user)) this.reset(user)
   }
-
+  /** 重置用户资料 */
   public reset(user: any) {
     each(user, (v, k) => this[k] = v)
   }
-
+  /** 登录用户 */
   public login(user: any) {
+    // 保存用户登录时间
+    user.$logintime = timestamp()
     this.reset(user)
     UserStore.set(this.$key, user)
     return this
   }
-
+  /** 登出用户 */
   public logout() {
     UserStore.remove(this.$key)
-    return this
+    this.id = 0
   }
 }
